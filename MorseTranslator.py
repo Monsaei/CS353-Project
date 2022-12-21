27073c921bb786f20cc1700b623043d6d418dee4
Monsaei <monsaei.delacruz@gmail.com>
2022-12-21 13:13:00 +0800

    Automata Project

---
+++ MorseTranslator.py
@@ -0,0 +1,156 @@
+from playsound import playsound
+import time
+
+# Class declaration para sa fsm kuno ket tree naman tlga
+class State:
+    def __init__(self, char='', left=None, right=None):
+        self.char= char
+        self.left= left
+        self.right= right
+
+# Functions for decoding Morse Code
+def morse_to_char(morse_fsm, morse_string, i=0):
+    if i==len(morse_string):
+          return morse_fsm.char
+    elif morse_string[i] == '.':
+          return morse_to_char(morse_fsm.left, morse_string, i+1)
+    else:
+          return morse_to_char(morse_fsm.right, morse_string, i+1)
+
+def decode_morse(morse_fsm, str):
+     decoded = ''
+     morse_string = str.split(' ')
+     for decode_string in morse_string:
+          if decode_string == '/':
+               decoded += ' '
+          else:
+               decoded += morse_to_char(morse_fsm, decode_string)
+     return decoded
+
+# Function for encoding a String to Morse Code
+def char_to_morse(morse_fsm, character, tmp):
+     if morse_fsm == None:
+          return False
+     elif morse_fsm.char == character:
+          return True
+     else:
+          if char_to_morse(morse_fsm.right,character, tmp) == True:
+               tmp.insert(0, "-")
+               return True
+          elif char_to_morse(morse_fsm.left,character, tmp) == True:
+               tmp.insert(0, ".")
+               return True
+
+# Function to play Morse Beep sounds
+def morse_beep (morse_code):
+     for morse_code in encoded:
+          if morse_code == '.':
+               playsound("MorseBeeps\dot.wav")
+          elif morse_code == '-':
+               playsound("MorseBeeps\dash.wav")
+          else:
+               time.sleep(0.3)
+
+# Morse FSM
+morse_fsm = State('',
+                State('E',
+                     State('I',
+                          State('S',
+                               State('H',
+                                    State('5'),
+                                    State('4')),
+                               State('V',
+                                    State(''),
+                                    State('3'))),
+                          State('U',
+                               State('F',
+                                    State(''),
+                                    State('')),
+                               State('',
+                                    State(''),
+                                    State('2')))),
+                     State('A',
+                          State('R',
+                               State('L',
+                                    State(''),
+                                    State('')),
+                               State('',
+                                    State('+'),
+                                    State(''))),
+                          State('W',
+                               State('P',
+                                    State(''),
+                                    State('')),
+                               State('J',
+                                    State(''),
+                                    State('1'))))),
+                State('T',
+                     State('N',
+                          State('D',
+                               State('B',
+                                    State('6'),
+                                    State('=')),
+                               State('X',
+                                    State('/'),
+                                    State(''))),
+                          State('K',
+                               State('C',
+                                    State(''),
+                                    State('')),
+                               State('Y',
+                                    State(''),
+                                    State('')))),
+                     State('M',
+                          State('G',
+                               State('Z',
+                                    State('7'),
+                                    State('')),
+                               State('Q',
+                                    State(''),
+                                    State(''))),
+                          State('O',
+                               State('',
+                                    State('8'),
+                                    State('')),
+                               State('',
+                                    State('9'),
+                                    State('0'))))))
+# Menu Function
+def menu():
+     print("[1] Translate Morse Code to Text")
+     print("[2] Translate English Word/s to Morse Code")
+     print("[0] Exit Program")
+
+menu()
+option = int(input("Enter an action: "))
+
+while option != 0:
+     if option == 1:
+          morse_string = input("Please enter a valid morse code (Separate words using '/'): ")
+          print(morse_string + "transltaed to word/s means:")
+          print(decode_morse(morse_fsm, morse_string))
+          print(input("Press any key to continue"))
+
+     elif option == 2:
+          encoded = ''
+          user_string = input("Please enter a message to be encoded in morse: ").upper()
+          for character in user_string:
+               tmp = []
+               char_to_morse(morse_fsm, character, tmp)
+               encoded_tmp = "".join(tmp)
+               encoded = encoded + encoded_tmp + " "
+          print(user_string + "will be translated to morse code as:")
+          print(encoded)
+          print("The conded message will be played in morse code please wait")
+          morse_beep(encoded)
+          print(input("Press any key to continue"))
+     else:
+          print("Invalid Option")
+     
+     print()
+     menu()
+     option = int(input("Enter an action: "))
+
+print("Thank you for using the program. babye")
+     
+
